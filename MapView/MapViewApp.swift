11dddc26bd739a2133ea869707df51631dec6d1c
Paulo Orquillo <olopsman@users.noreply.github.com>
2023-04-11 07:59:59 +1200

    Initial Commit

---
+++ MapView/MapViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewApp.swift
+//  MapView
+//
+//  Created by Paulo Orquillo on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MapViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
